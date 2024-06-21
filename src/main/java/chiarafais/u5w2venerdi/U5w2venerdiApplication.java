6b1d51779d8e0614c0d58f09e63d7b9716ffdb2d
chiarafais <chiara0915@gmail.com>
2024-06-21 10:38:26 +0200

    first commit

---
+++ src/main/java/chiarafais/u5w2venerdi/U5w2venerdiApplication.java
@@ -0,0 +1,13 @@
+package chiarafais.u5w2venerdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5w2venerdiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5w2venerdiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chiarafais/u5w2venerdi/U5w2venerdiApplicationTests.java
@@ -0,0 +1,13 @@
+package chiarafais.u5w2venerdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5w2venerdiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
